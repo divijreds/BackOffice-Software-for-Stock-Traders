109eaab06bc381899078c6f9997f4195eb536fca
divijreds <34851812+divijreds@users.noreply.github.com>
2019-05-21 13:52:45 +0530

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+label_1 = Label(root,text="User ID")
+label_2 = Label(root,text="Client ID")
+entry_1 = Entry(root)
+entry_2 = Entry(root)
+label_1.grid(row=0,column=0)
+label_2.grid(row=0,column=1)
+entry_1.grid(row=1,column=0)
+entry_2.grid(row=1,column=1)
+root.mainloop()
